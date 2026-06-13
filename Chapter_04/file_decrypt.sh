e1889e6da6b4123fc2b1a8db4f6125b0873c0ed0
donniet4 <55449545+donniet4@users.noreply.github.com>
2026-06-13 09:18:46 -0400

    Add files via upload

---
+++ Chapter_04/file_decrypt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+inputFile=$1
+outputFile=$2
+
+if [[ $# -ne 2 ]]; then
+	echo "This script requires two arguments."
+	exit 1
+fi
+
+if [[ -f "$outputFile" ]]; then
+	echo "This file already exists."
+	exit 1
+fi
+
+openssl enc -d -aes-256-ctr -md sha-512 -pbkdf2 -iter 600000 -in "$inputFile" -out "$outputFile"
